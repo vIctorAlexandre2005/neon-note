f7e9aeb89d1273d2e49470a2bf996823a3562c4f
Victor Alexandre <vtrm2005@gmail.com>
2024-05-22 16:55:54 -0300

    PWA ok!

--- neon-note-project/src/pages/index.tsx
+++ neon-note-project/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { LoginComponent } from "@/components/LoginComponent";
 import { NeonNote } from "@/components/Note";
 import { useTheme } from "@/components/ThemeDark";
 import { auth } from "@/services/firebase";
+import { Box, Button, Img, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -64,6 +65,54 @@ export default function Home() {
 
   return (
     <div className={`${darkMode ? 'bg-black-900' : 'bg-neon-50'}`}>
+      {installPrompt ? (
+          <Modal isCentered isOpen={isOpenModal} onClose={onClose}>
+            <ModalOverlay
+              onClick={onClose}
+              width={"100%"}
+              height={"100%"}
+              background={"#00000090"}
+            />
+            <ModalContent
+              transition={"0.2s"}
+              background={'white'}
+              width={"24rem"}
+            >
+              <ModalHeader marginBottom={"2rem"} display={"flex"}
+                alignItems={"center"} justifyContent={"center"}
+              >
+                <Box gap={"12px"} display={"flex"}
+                  alignItems={"center"} justifyContent={"center"}
+                >
+                  <Text fontWeight={"600"} color={"#004aff"} fontSize={"1.1rem"}>
+                    Tenha o Neon Note em seu dispositivo!
+                  </Text>
+                </Box>
+              </ModalHeader>
+              <ModalBody display={"flex"} justifyContent={"center"}>
+                <Img src="/login.svg" height={180} width={180} />
+              </ModalBody>
+              <ModalFooter width={"100%"} marginTop={"1rem"}>
+                <Button
+                  width={"100%"}
+                  padding={"0.5rem"}
+                  border={"none"}
+                  borderRadius={"6px"}
+                  background={"#004aff"}
+                  color={"white"}
+                  fontWeight={"bold"}
+                  onClick={handleInstall}
+                  _hover={{ background: '#0229a6', transition: '0.3s' }}
+                  gap={2}
+                >
+                  Baixar
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        ) : (
+          ''
+        )}
       <NeonNote />
     </div>
   );
