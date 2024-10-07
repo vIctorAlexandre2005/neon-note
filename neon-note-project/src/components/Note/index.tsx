d4e4726f665e8a91feb1cb5096f8967440d09018
Victor Alexandre <vtrm2005@gmail.com>
2024-10-06 21:38:29 -0300

    feat MOBILE: delete doc and saving when write

--- neon-note-project/src/components/Note/index.tsx
+++ neon-note-project/src/components/Note/index.tsx
@@ -6,8 +6,11 @@ import { useEffect, useState } from "react";
 import { useContextNoteData } from "../Context/NoteContext";
 import { useContextGlobal } from "../Context";
 import { Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerHeader, ModalBody, ModalHeader, Popover, PopoverAnchor, PopoverBody, PopoverContent, PopoverHeader, PopoverTrigger, useDisclosure } from "@chakra-ui/react";
-import { BiArrowBack, BiDotsVertical, BiTrash } from "react-icons/bi";
+import { BiArrowBack, BiCheck, BiDotsVertical, BiTrash } from "react-icons/bi";
 import { InputComponent } from "../InputComponent";
+import { debounce } from "@/utils/debounce";
+import { db } from "@/services/firebase";
+import FadeIn from "../Effects/FadeIn";
 
 export function NeonNote() {
 
@@ -19,16 +22,35 @@ export function NeonNote() {
   const { setTitleNote, setTextNote, noteList, updateNote, deleteNote } = useContextNoteData();
 
   const activeNoteId = noteList.find((note) => note.id === activeNote); // Encontra a nota ativa
+  const [saving, setSaving] = useState(false); // Inicia como falso
+  const [saved, setSaved] = useState(false);
+
+  const debouncedUpdateNote = debounce((id: number, updatedFields: any) => {
+    setSaving(true); // Define saving como verdadeiro antes de tentar atualizar
+    const noteRef = db.collection("notes").doc(id.toString()); // Supondo que 'id' seja a chave do documento
+    noteRef.update(updatedFields)
+      .then(() => {
+        setSaving(false);
+        setSaved(true);
+        setTimeout(() => setSaved(false), 2000); // Limpa a mensagem "Salvo!" após 2 segundos
+      })
+      .catch((error) => {
+        console.error("Erro ao atualizar a nota:", error);
+        setSaving(false); // Se houver erro, não esqueça de parar o estado de salvando
+      });
+  }, 500);  // Debounce com 500ms de atraso
 
   // Funções de handle para capturar as mudanças nos inputs
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitleNote(e.target.value);
-    updateNote(activeNote, { title: e.target.value });
+    updateNote(activeNote, { title: e.target.value }); // Atualiza no estado local
+    debouncedUpdateNote(activeNote, { title: e.target.value }); // Atualiza no Firebase
   };
 
   const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTextNote(e.target.value);
-    updateNote(activeNote, { text: e.target.value });
+    updateNote(activeNote, { text: e.target.value }); // Atualiza no estado local
+    debouncedUpdateNote(activeNote, { text: e.target.value }); // Atualiza no Firebase
   };
 
   return (
@@ -40,85 +62,82 @@ export function NeonNote() {
       {isOpen && isMobile && (
         <Drawer isOpen={isOpen} onEsc={onClose} closeOnEsc placement="left" size={"full"} onClose={onClose}>
           <DrawerContent bg={darkMode ? "#0f172a" : "#d2e8ff"}>
-            {activeNoteId && (
-              <div className="flex flex-col h-full">
-                <ModalHeader>
-                  <div className="flex items-center justify-between">
+          {activeNoteId && (
+        <div className="flex flex-col h-full gap-4">
+          <div className="flex p-4 justify-between items-center">
 
-                    <button onClick={onClose}>
-                      <BiArrowBack size={20} color={darkMode ? "#d2e8ff" : "#0f172a"} />
-                    </button>
+            <button onClick={onClose}>
+              <BiArrowBack size={24} color={darkMode ? "white" : "black"} />
+            </button>
 
-                    <Popover isOpen={isOpenPopover} onClose={onClosePopover}>
-                      <PopoverTrigger>
-                        <button onClick={onOpenPopover}>
-                          <BiDotsVertical size={20} color={darkMode ? "#d2e8ff" : "#0f172a"} />
-                        </button>
-                      </PopoverTrigger>
-
-                      <PopoverContent w={"fit-content"} bg={darkMode ? "#1e293b" : "#d2e8ff"}>
-                        <PopoverBody>
-                          <button
-                            className={` flex gap-2 text-base items-center ${darkMode ? "text-white hover:text-red-500 duration-200 transition-all" : "text-black-800 hover:text-red-500 duration-200 transition-all"}`}
-                            onClick={() => {
-                              deleteNote(activeNoteId.id);
-                              onClosePopover();
-                              onClose();
-                            }}
-                          >
-                            Excluir <BiTrash size={20} />
-                          </button>
-                        </PopoverBody>
-                      </PopoverContent>
-                    </Popover>
+            <div className="flex gap-2 items-center justify-start">
+              {!saving && saved && (
+                <>
+                  <FadeIn>
+                  <div className="flex gap-1 items-center">
+                  <p className="text-white">Salvo</p> <BiCheck size={24} className="text-green-400" />
                   </div>
-                </ModalHeader>
-
-                <ModalBody onClick={onClosePopover}>
-
-                  <InputComponent
-                    className={`
+                  </FadeIn>
+                </>
+              )}
+            </div>
+            <div className="flex gap-2 items-center justify-end">
+              <button
+                className={`${darkMode ? "text-white hover:text-red-500 duration-200 transition-all" : "text-black-800 hover:text-red-500 duration-200 transition-all"}`}
+                onClick={() => {
+                  deleteNote(activeNoteId.id);
+                }}
+              >
+                <BiTrash size={24} />
+              </button>
+            </div>
+          </div>
+          {/* Título */}
+          <div className="flex items-center">
+            <InputComponent
+              className={`
                 border-none
                 bg-transparent
                 rounded-md 
                 ${darkMode ? "text-white" : "text-black-800"}
-                xs:px-2 md:px-4 
-                xs:py-0 md:py-2 
+                px-4 
+                py-2 
                 focus:outline-none
-                xs:placeholder:text-xl   md:placeholder:text-3xl 
+                placeholder:text-3xl 
                 ${darkMode ? "placeholder:opacity-50" : "placeholder:opacity-95"}
-                xs:text-xl md:text-3xl
+                text-3xl
                 font-semibold
                 w-full
               `}
-                    placeholder="Título"
-                    value={activeNoteId.title}
-                    onChange={handleTitleChange}
-                  />
+              placeholder="Título"
+              value={activeNoteId.title}
+              onChange={handleTitleChange} // Atualiza o título
+            />
+          </div>
 
-                  <textarea
-                    placeholder="Criar nota..."
-                    value={activeNoteId.text}
-                    onChange={handleTextChange}
-                    className={`
+          {/* Textarea */}
+          <textarea
+            placeholder="Criar nota..."
+            value={activeNoteId.text}
+            onChange={handleTextChange}
+            className={`
                 border-none
                 resize-none
                 mt-2
                 w-full
                 h-full
                 bg-transparent
-                xs:px-2 md:px-4 
-                xs:py-0 md:py-2
-                xs:text-sm md:text-lg
+                px-4 
+                text-lg
+                py-2 
                 ${darkMode ? "text-white" : "text-black-700"}
                 placeholder:text-start
                 focus:outline-none  
                 ${darkMode ? "placeholder:opacity-50" : "placeholder:opacity-95"}
               `}
-                  />
-                </ModalBody>
-              </div>
-            )}
+          />
+        </div>
+      )}
           </DrawerContent>
         </Drawer>
       )}
