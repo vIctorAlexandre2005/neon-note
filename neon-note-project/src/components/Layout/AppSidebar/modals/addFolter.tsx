0cf5a45c4b7c36a06f5c8546f377a60080c2518f
Victor Alexandre <vtrm2005@gmail.com>
2024-10-25 21:44:22 -0300

    feat: open and closed folders

---
+++ neon-note-project/src/components/Layout/AppSidebar/modals/addFolter.tsx
@@ -0,0 +1,56 @@
+import { ButtonComponent } from "@/components/common/Button";
+import { InputComponent } from "@/components/common/InputField";
+import { ModalComponent } from "@/components/Modals/modal";
+
+interface Props {
+    isOpenAddFolder: boolean;
+    onCloseAddFolder: () => void;
+    newFolderName: string;
+    setNewFolderName: (value: string) => void;
+    handleAddFolder: () => void;
+    darkMode: boolean;
+}
+
+export function AddFolderModal({
+    isOpenAddFolder,
+    onCloseAddFolder,
+    newFolderName,
+    setNewFolderName,    
+    handleAddFolder,
+    darkMode
+}: Props) {
+
+    return (
+        <ModalComponent onClose={onCloseAddFolder} isOpen={isOpenAddFolder}>
+          <div className='flex flex-col p-4 gap-4'>
+            <h1
+              className={`text-2xl font-semibold ${darkMode ? 'text-white' : 'text-black-800'}`}
+            >
+              Nome da pasta
+            </h1>
+            <InputComponent
+              placeholder='Inserir nome da pasta'
+              className={`w-full p-2 border border-neon-400 focus:border-2 outline-none rounded-lg ${darkMode ? 'text-white bg-black-900' : 'text-black-800 bg-white'} `}
+              value={newFolderName}
+              onChange={e => setNewFolderName(e.target.value)}
+            />
+
+            <div className='flex gap-4'>
+              <ButtonComponent
+                onClick={() => {
+                  handleAddFolder();
+                  onCloseAddFolder();
+                }}
+                text='Adicionar'
+                className='bg-neon-400 text-white text-center w-full rounded-lg'
+              />
+              <ButtonComponent
+                onClick={onCloseAddFolder}
+                text='Cancelar'
+                className='bg-red-500 w-full text-white text-center rounded-lg'
+              />
+            </div>
+          </div>
+        </ModalComponent>
+    )
+}
--- neon-note-project/src/components/Layout/AppSidebar/secondarySidebar.tsx
+++ neon-note-project/src/components/Layout/AppSidebar/secondarySidebar.tsx
@@ -15,6 +15,8 @@ import {
   IoIosArrowDropright,
   IoIosArrowForward,
 } from 'react-icons/io';
+import { AddFolderModal } from './modals/addFolter';
+import { AddFolderItemModal } from './modals/addItemFolder';
 
 interface SidebarProps {
   darkMode: boolean;
@@ -29,7 +31,7 @@ interface Folder {
 export function SecondarySidebar({ darkMode }: SidebarProps) {
   const router = useRouter();
 
-  const [openSubFolder, setOpenSubFolder] = useState(false);
+  const [openSubFolder, setOpenSubFolder] = useState<null | number>(null);
   const [folders, setFolders] = useState<Folder[]>([]);
   const [newFolderName, setNewFolderName] = useState('');
   const [newItemName, setNewItemName] = useState('');
@@ -47,9 +49,16 @@ export function SecondarySidebar({ darkMode }: SidebarProps) {
     onClose: onCloseAddItem,
   } = useDisclosure();
 
-  function openSubFolders() {
-    setOpenSubFolder(!openSubFolder);
-  }
+  function openSubFolders(id: number) {
+    setOpenSubFolder((prev) => {
+      console.log(prev, id);
+      if (prev === id) {
+        return null;
+      } else {
+        return id;
+      }
+    });
+  };
 
   const handleAddFolder = () => {
     if (newFolderName.trim()) {
@@ -77,7 +86,9 @@ export function SecondarySidebar({ darkMode }: SidebarProps) {
   };
 
   return (
-    <div className={`flex-none w-full ${darkMode ? 'bg-slate-900' : 'bg-slate-50'}`}>
+    <div
+      className={`flex-none w-full ${darkMode ? 'bg-slate-900' : 'bg-slate-50'}`}
+    >
       <div className={`flex-col mt-6 gap-4 flex`}>
         <div className='flex justify-between p-2 items-center'>
           <h1 className='text-2xl font-bold text-black-600'>Minhas pastas</h1>
@@ -88,119 +99,92 @@ export function SecondarySidebar({ darkMode }: SidebarProps) {
           />
         </div>
 
-        <div className='flex flex-col gap-2'>
+        <div className='flex flex-col gap-1'>
           {folders.length === 0 && (
             <div className='flex flex-col justify-center mt-20 items-center'>
-              <Image alt='' src={'/emptyFolder1.svg'} width={200} height={200} />
-              <h1 className={`text-md font-semibold ${darkMode ? 'text-black-200' : 'text-black-700'}`}>
+              <Image
+                alt=''
+                src={'/emptyFolder1.svg'}
+                width={200}
+                height={200}
+              />
+              <h1
+                className={`text-md font-semibold ${darkMode ? 'text-black-200' : 'text-black-700'}`}
+              >
                 Você ainda não possui pastas
               </h1>
             </div>
           )}
 
-          {folders.length > 0 && (
+          {folders.length > 0 &&
             folders.map(folder => (
-              <div className='flex flex-col p-2'>
+              <div className='flex flex-col pl-4'>
                 <div
                   className='w-full mb-2 flex justify-between items-center'
                   onClick={() => {
                     setSelectedFolderId(folder.id);
                   }}
                 >
-                  <div 
-                    className={`flex gap-2 ${darkMode ? 'text-black-200' : 'text-black-700'} w-full`} 
-                    onClick={openSubFolders}>
-                    {!openSubFolder ? <IoIosArrowForward /> : <IoIosArrowDown />}
+                  <div
+                    className={`flex gap-2 ${darkMode ? 'text-black-200' : 'text-black-700'} w-full`}
+                    onClick={() => openSubFolders(folder.id)}
+                  >
+                    {openSubFolder !== folder.id ? (
+                      <IoIosArrowForward />
+                    ) : (
+                      <IoIosArrowDown />
+                    )}
                     <FaFolder size={18} />
-                    <h1 className={`text-md font-bold`}>
-                      {folder.name}
-                    </h1>
+                    <h1 className={`text-md font-bold`}>{folder.name}</h1>
                   </div>
                   <div className='flex gap-4 -z-0 items-center justify-between'>
                     <ButtonComponent
                       onClick={onOpenAddItem}
                       icon={<BiPlus size={18} />}
-                      className= {`hover:bg-neon-400 hover:text-white ${darkMode ? 'text-black-200' : 'text-black-700'} rounded-full`}
+                      className={`hover:bg-neon-400 hover:text-white ${darkMode ? 'text-black-200' : 'text-black-700'} rounded-full`}
                     />
                   </div>
                 </div>
-  
+
                 {openSubFolder && folder.id === selectedFolderId && (
                   <div className='flex justify-center flex-col gap-2'>
                     {folder.items.map((item, index) => (
                       <FadeIn>
                         <div>
-                        <h1 className={`text-md text-center font-semibold ${darkMode ? 'text-black-100' : 'text-black-800'}`}>
-                          {item}
-                        </h1>
-                      </div>
+                          <h1
+                            className={`text-md text-center font-semibold ${darkMode ? 'text-black-100' : 'text-black-800'}`}
+                          >
+                            {item}
+                          </h1>
+                        </div>
                       </FadeIn>
                     ))}
                   </div>
                 )}
               </div>
-            ))
-          )}
+            ))}
         </div>
       </div>
       {isOpenAddFolder && (
-        <ModalComponent onClose={onCloseAddFolder} isOpen={isOpenAddFolder}>
-          <div className='flex flex-col p-4 gap-4'>
-            <h1 className={`text-2xl font-semibold ${darkMode ? 'text-white' : 'text-black-800'}`}>Nome da pasta</h1>
-            <InputComponent
-              placeholder='Inserir nome da pasta'
-              className={`w-full p-2 border border-neon-400 focus:border-2 outline-none rounded-lg ${darkMode ? 'text-white bg-black-900' : 'text-black-800 bg-white'} `}
-              value={newFolderName}
-              onChange={e => setNewFolderName(e.target.value)}
-            />
-
-            <div className='flex gap-4'>
-              <ButtonComponent
-                onClick={() => {
-                  handleAddFolder();
-                  onCloseAddFolder();
-                }}
-                text='Adicionar'
-                className='bg-neon-400 text-white text-center w-full rounded-lg'
-              />
-              <ButtonComponent
-                onClick={onCloseAddFolder}
-                text='Cancelar'
-                className='bg-red-500 w-full text-white text-center rounded-lg'
-              />
-            </div>
-          </div>
-        </ModalComponent>
+        <AddFolderModal 
+          darkMode={darkMode}
+          handleAddFolder={handleAddFolder}
+          isOpenAddFolder={isOpenAddFolder}
+          newFolderName={newFolderName}
+          onCloseAddFolder={onCloseAddFolder}
+          setNewFolderName={setNewFolderName}
+        />
       )}
 
       {isOpenAddItem && (
-        <ModalComponent onClose={onCloseAddItem} isOpen={isOpenAddItem}>
-          <div className='flex flex-col p-4 gap-4'>
-            <h1 className={`text-2xl font-semibold ${darkMode ? 'text-white' : 'text-black-800'}`}>Insira o nome do item</h1>
-            <InputComponent
-              placeholder='Nome do item onde ficará as anotações'
-              className={`w-full p-2 border border-neon-400 focus:border-2 outline-none rounded-lg ${darkMode ? 'text-white bg-black-900' : 'text-black-800 bg-white'} `}
-              value={newItemName}
-              onChange={e => setNewItemName(e.target.value)}
-            />
-
-            <div className='flex gap-4'>
-              <ButtonComponent
-                onClick={() => {
-                  handleAddItem();
-                  onCloseAddItem();
-                }}
-                text='Adicionar'
-                className='bg-neon-400 text-white text-center w-full rounded-lg'
-              />
-              <ButtonComponent
-                onClick={onCloseAddItem}
-                text='Cancelar'
-                className='bg-red-500 w-full text-white text-center rounded-lg'
-              />
-            </div>
-          </div>
-        </ModalComponent>
+        <AddFolderItemModal
+          darkMode={darkMode}
+          handleAddItem={handleAddItem}
+          isOpenAddItem={isOpenAddItem}
+          newItemName={newItemName}
+          onCloseAddItem={onCloseAddItem}
+          setNewItemName={setNewItemName}
+        />
       )}
     </div>
   );
