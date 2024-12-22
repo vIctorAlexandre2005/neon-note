b876876b082debab6c4a6100e659f565aa7a1015
20051972mr@ <vtrm2005@gmail.com>
2024-12-21 21:20:12 -0300

    fix: salvando anotacoes no banco de dados corretamente quando no mobile

--- neon-note-project/src/components/Notes/View/index.tsx
+++ neon-note-project/src/components/Notes/View/index.tsx
@@ -39,29 +39,22 @@ export function NeonNote() {
 
   const { selectedItem } = useContextNoteData();
 
-  const debouncedUpdateNote = debounce(
-    async (id: string, updatedFields: any) => {
-      setSaving(true); // Inicia o estado de salvamento
-
+  const debouncedUpdateNote = debounce(async (id: string, updatedFields: any) => {
+      setSaving(true);
       try {
-        const noteRef = doc(db, 'users', user.uid, 'notes', id);
-
+        const noteRef = doc(db, `users/${user?.uid}/folders/${selectedFolderId}/notes/${id}`);
         const sanitizedFields: any = Object.fromEntries(
           Object.entries(updatedFields).filter(([_, v]) => v !== undefined)
         );
-
         await updateDoc(noteRef, sanitizedFields);
         setSaved(true);
-
         setTimeout(() => setSaved(false), 2000);
       } catch (error) {
-        console.error('Erro ao atualizar a nota:', error);
+        console.error("Erro ao atualizar a nota:", error);
       } finally {
         setSaving(false);
       }
-    },
-    500
-  );
+    }, 500);
 
   // Funções de handle para capturar as mudanças nos inputs
   const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
