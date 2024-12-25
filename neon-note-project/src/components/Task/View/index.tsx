dae26e63a28a74a5b299f618e2fb63d86a2c55a0
20051972mr@ <vtrm2005@gmail.com>
2024-12-25 19:12:24 -0300

    Sem outras paginas

--- neon-note-project/src/components/Task/View/index.tsx
+++ neon-note-project/src/components/Task/View/index.tsx
@@ -7,6 +7,8 @@ import { useContextNoteData } from '@/components/Notes/Context/NoteContext';
 import { useContextGlobal } from '@/Context';
 import { useSidebarCardsNote } from '../../Notes/ViewModel/useSidebarCardsNote';
 import { SidebarTasksReuse } from '../../common/Task/SidebarTaskReuse';
+import { ButtonComponent } from '@/components/common/Button';
+import { IoOptionsSharp } from 'react-icons/io5';
 
 export function TaskComponent() {
   const { noteList } = useSidebarCardsNote();
@@ -14,13 +16,21 @@ export function TaskComponent() {
   const { selectedItem } = useContextNoteData();
 
   return (
-    <div className='flex flex-col h-full gap-2 p-4'>
+    <div className='flex flex-col h-full gap-2 p-2'>
+      <div className='flex justify-between items-center'>
       <h1
-        className={`text-2xl ${darkMode ? 'text-gray-300' : 'text-black-800'} font-bold`}
+        className={`text-xl ${darkMode ? 'text-gray-300' : 'text-black-800'} font-bold`}
       >
-        {' '}
-        {selectedItem}{' '}
+        {selectedItem}
       </h1>
+
+      <ButtonComponent 
+        text='Filtrar' 
+        className={`text-xl gap-2 ${darkMode ? 'text-gray-300' : 'text-black-800'} hover:bg-gray-200 transition duration-300 rounded-full`} 
+        icon={<IoOptionsSharp size={32} 
+        color={darkMode ? 'white' : 'black'} />} 
+      />
+      </div>
       <div className='flex gap-4 h-full'>
         <SidebarTasksReuse
           arrayTasks={noteList}
@@ -30,6 +40,7 @@ export function TaskComponent() {
           statusIconColorBackground='blue'
           colorProgressStatusBar='blue'
           numberTasksStatusDone={0}
+          thereIsNoButtonCreateTaskInSidebar={true}
         />
 
         <SidebarTasksReuse
@@ -40,6 +51,7 @@ export function TaskComponent() {
           statusIconColorBackground='orange'
           colorProgressStatusBar='orange'
           numberTasksStatusDone={4}
+          thereIsNoButtonCreateTaskInSidebar={true}
         />
 
         <SidebarTasksReuse
@@ -50,6 +62,7 @@ export function TaskComponent() {
           statusIconColorBackground='green'
           colorProgressStatusBar='green'
           numberTasksStatusDone={noteList?.length}
+          thereIsNoButtonCreateTaskInSidebar={false}
         />
       </div>
     </div>
